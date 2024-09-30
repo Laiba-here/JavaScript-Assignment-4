07a15b1c1513951886f46d13752ee11f0ceb2ae8
Laiba-here <nazlaiba230@gmail.com>
2024-09-29 20:53:10 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//         .... QUESTION 1 ....
+// let age = quantity = number = 46;
+
+//         .... QUESTION 2 ....
+//        Legal VariableNames:
+
+// firstName
+// _super
+// $price
+// vanish2
+// camelCaseName
+// snakCase
+
+//         Illegal Variable Names:
+
+
+//  3smart(Cannot start with a number)
+//  first-name (Hyphens are not allowed)
+//  var (Cannot use reserved keywords)
+//  @name (Special characters like @ are not allowed)
+//  class name (Spaces are not allowed)
